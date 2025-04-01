6064a0ddc1e4fa97acd2d082dc4ed72aff401e5b
Paulo <paulodiasred@gmail.com>
2025-03-31 22:23:27 -0300

    variaveis, metodos, escopos em Java

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,20 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int a, b;
+        a = 5;
+        b = 5;
+
+        // String resultado = "";
+
+        // if (a == b)
+        //     resultado = "verdadeiro";
+        // else
+        //     resultado = "falso";
+
+        //resumido
+        String resultado = a==b? "verdadeiro" : "falso";
+        
+
+        System.out.println("a é igual a b? " + resultado);
+    }
+}
